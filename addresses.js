120e99afab80671825c9f0fd414d3627b407b0ce
rixonic <franco.j.cejas@gmail.com>
2024-05-21 19:53:48 -0300

    Initial commit

---
+++ addresses.js
@@ -0,0 +1,116 @@
+module.exports = [
+    { 
+        nombre: 'Stock Reactivos Autoanalizador', 
+        sensorId: 'A_5_12',
+        labId: 'EC-123', 
+        tempAddr: 0, 
+        highAddr: 100, 
+        lowAddr: 102 
+    },{
+        nombre: 'Stock Bacteriologia y Microbiologia 1', 
+        sensorId: 'A_5_58', 
+        labId: 'EC-123',
+        tempAddr: 2, 
+        highAddr: 104, 
+        lowAddr: 106 
+    },{
+        nombre: 'Stock Bacteriologia y Microbiologia 2', 
+        sensorId: 'A_5_59', 
+        labId: 'EC-123',
+        tempAddr: 4, 
+        highAddr: 108, 
+        lowAddr: 110 
+    },{
+        nombre: 'Freezer 1', 
+        sensorId: 'A_5_13', 
+        labId: 'EC-123',
+        tempAddr: 6, 
+        highAddr: 112, 
+        lowAddr: 114 
+    },{
+        nombre: 'Freezer 2', 
+        sensorId: 'A_5_14', 
+        labId: 'EC-112',
+        tempAddr: 8, 
+        highAddr: 116, 
+        lowAddr: 118 
+    },{
+        nombre: 'Cultivo Microbiologia 2', 
+        sensorId: 'A_5_19', 
+        labId: '',
+        tempAddr: 10, 
+        highAddr: 120, 
+        lowAddr: 122 
+    },{
+        nombre: 'Cultivo Microbiologia 1', 
+        sensorId: 'A_5_18', 
+        labId: '',
+        tempAddr: 12, 
+        highAddr: 124, 
+        lowAddr: 126 
+    },{
+        nombre: 'Microbiologia', 
+        sensorId: 'A_5_10', 
+        labId: '',
+        tempAddr: 14, 
+        highAddr: 128, 
+        lowAddr: 130 
+    },{
+        nombre: 'Reactivos COBAS', 
+        sensorId: 'A_5_08', 
+        labId: '',
+        tempAddr: 16, 
+        highAddr: 132, 
+        lowAddr: 134 
+    },{
+        nombre: 'Sueros', 
+        sensorId: 'A_5_07', 
+        labId: '',
+        tempAddr: 18, 
+        highAddr: 136, 
+        lowAddr: 138 
+    },{
+        nombre: 'Proteinas y Seriologia', 
+        sensorId: 'A_5_09', 
+        labId: '',
+        tempAddr: 22, 
+        highAddr: 144, 
+        lowAddr: 146 
+    },{
+        nombre: 'Geles', 
+        sensorId: 'A_5_20', 
+        labId: '',
+        tempAddr: 24, 
+        highAddr: 148, 
+        lowAddr: 150 
+    },{
+        nombre: 'Ultrafreezer', 
+        sensorId: 'A_5_54', 
+        labId: '',
+        tempAddr: 26, 
+        highAddr: 152, 
+        lowAddr: 154 
+    },{
+        nombre: 'Muestras Biologia Molecular', 
+        sensorId: 'A_5_57', 
+        labId: '',
+        tempAddr: 28, 
+        highAddr: 156, 
+        lowAddr: 158 
+    },{
+        nombre: 'Master MIX', 
+        sensorId: 'A_5_46', 
+        labId: '',
+        tempAddr: 28, 
+        highAddr: 160, 
+        lowAddr: 162 
+    },{
+        nombre: 'Amplificador', 
+        sensorId: 'A_5_11', 
+        labId: '',
+        tempAddr: 28, 
+        highAddr: 164, 
+        lowAddr: 166 
+    }
+    // Añade aquí el resto de los sensores según tu configuración
+  ];
---
+++ app.js
@@ -0,0 +1,259 @@
+const express = require('express');
+const jsmodbus = require('jsmodbus');
+const WebSocket = require('ws');
+const fs = require('fs');
+const path = require('path');
+const net = require('net');
+
+const sensors = require('./addresses');
+
+// Configuración de Express
+const app = express();
+const port = 3001;
+
+const results = [];
+
+// Configuración de Modbus TCP
+const socket = new net.Socket();
+const client = new jsmodbus.client.TCP(socket, 1); // unitId=1
+const options = {
+  'host': '192.168.90.235', // Cambia esto a la IP de tu PLC
+  'port': 502
+};
+
+function connectModbus() {
+  return new Promise((resolve, reject) => {
+    socket.connect(options, resolve);
+    socket.on('error', reject);
+  });
+}
+// Cargar el archivo de configuración
+const configPath = path.join(__dirname, 'config', 'time.json');
+let config;
+
+try {
+  const data = fs.readFileSync(configPath, 'utf8');
+  config = JSON.parse(data);
+} catch (err) {
+  console.error('Error leyendo el archivo de configuración:', err);
+  process.exit(1);
+}
+
+const alarmStates = {};
+sensors.forEach(sensor => {
+  const alarmTime = config[sensor.sensorId] || 0;
+  alarmStates[sensor.sensorId] = { remainingTime: alarmTime * 60, alert: false, startTime: null }; // Convertimos minutos a segundos
+});
+
+// Función para convertir registros Modbus a float
+function registersToFloat(registers) {
+  const buffer = Buffer.alloc(4);
+  buffer.writeUInt16BE(registers[0], 0);
+  buffer.writeUInt16BE(registers[1], 2);
+  return buffer.readFloatBE(0);
+}
+
+function floatToRegisters(value) {
+  const buffer = Buffer.alloc(4);
+  buffer.writeFloatBE(value, 0);
+  return [buffer.readUInt16BE(0), buffer.readUInt16BE(2)];
+}
+
+// Función para leer los datos del PLC y armar el JSON
+async function initialReadingSensors() {
+  for (let sensor of sensors) {
+    try {
+      // Leer registros TEMP, HIGH y LOW del PLC
+      const tempRegs = await client.readHoldingRegisters(sensor.tempAddr, 2);
+      const highRegs = await client.readHoldingRegisters(sensor.highAddr, 2);
+      const lowRegs = await client.readHoldingRegisters(sensor.lowAddr, 2);
+
+      const temp = registersToFloat(tempRegs.response._body._values);
+      const high = registersToFloat(highRegs.response._body._values);
+      const low = registersToFloat(lowRegs.response._body._values);
+
+      const time = config[sensor.sensorId] || 0;
+      // Crear el objeto del sensor
+      results.push({
+        nombre: sensor.nombre,
+        sensorId: sensor.sensorId,
+        temp: temp.toFixed(2),
+        high: high.toFixed(2),
+        low: low.toFixed(2),
+        alert: false,
+        time: time * 60
+      });
+    } catch (err) {
+      console.error(`Error leyendo los datos del PLC para el sensor ${sensor.sensorId}:`, err);
+    }
+  }
+}
+
+
+async function readSensors() {
+  const currentTime = Date.now() / 1000; // Convertir a segundos
+
+  for (let sensor of sensors) {
+    try {
+      // Leer registro TEMP del PLC
+      const tempRegs = await client.readHoldingRegisters(sensor.tempAddr, 2);
+      const temp = registersToFloat(tempRegs.response._body._values);
+
+      const sensorIndex = results.findIndex(result => result.sensorId === sensor.sensorId);
+
+      if (sensorIndex !== -1) {
+        // Actualizar el valor de temp
+        results[sensorIndex].temp = temp.toFixed(2);
+
+        // Obtener umbrales y tiempos del sensor
+        const { high, low } = results[sensorIndex];
+        const highThreshold = parseFloat(high);
+        const lowThreshold = parseFloat(low);
+
+        // Verificar si la temperatura excede los umbrales
+        const isExceeded = (temp > highThreshold || temp < lowThreshold);
+
+        if (isExceeded) {
+          // Si excede, iniciar o continuar el contador
+          if (!alarmStates[sensor.sensorId].startTime) {
+            alarmStates[sensor.sensorId].startTime = currentTime;
+          }
+          const elapsedTime = currentTime - alarmStates[sensor.sensorId].startTime;
+
+          if (elapsedTime >= alarmStates[sensor.sensorId].remainingTime) {
+            results[sensorIndex].alert = true;
+            results[sensorIndex].time = 0;
+          } else {
+            results[sensorIndex].time = Math.round(alarmStates[sensor.sensorId].remainingTime - elapsedTime);
+          }
+        } else {
+          // Si no excede, resetear el contador y la alarma
+          results[sensorIndex].alert = false;
+          results[sensorIndex].time = alarmStates[sensor.sensorId].remainingTime;
+          alarmStates[sensor.sensorId].startTime = null;
+        }
+      }
+    } catch (err) {
+      console.error(`Error leyendo los datos del PLC para el sensor ${sensor.sensorId}:`, err);
+    }
+  }
+}
+
+async function updateSensorThreshold(sensorId, thresholdType, value) {
+  const sensor = sensors.find(s => s.sensorId === sensorId);
+  if (!sensor) {
+    throw new Error(`Sensor with ID ${sensorId} not found`);
+  }
+
+  const registers = floatToRegisters(value);
+  const address = thresholdType === 'high' ? sensor.highAddr : sensor.lowAddr;
+
+  await client.writeRegisters(address, registers);
+
+  const resultIndex = results.findIndex(r => r.sensorId === sensorId);
+  if (resultIndex !== -1) {
+    results[resultIndex][thresholdType] = value.toFixed(2);
+  }
+}
+
+async function updateSensorTime(sensorId, time) {
+  const configPath = path.join(__dirname, 'config', 'time.json');
+  config[sensorId] = time;
+
+  try {
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf8');
+  } catch (err) {
+    throw new Error('Error writing to config file');
+  }
+
+  const resultIndex = results.findIndex(r => r.sensorId === sensorId);
+  if (resultIndex !== -1) {
+    results[resultIndex].time = time * 60;
+  }
+
+  if (alarmStates[sensorId]) {
+    alarmStates[sensorId].remainingTime = time * 60;
+  }
+}
+
+async function startServer() {
+  await connectModbus(); 
+  await initialReadingSensors(); // Espera a que se complete la lectura inicial
+
+  const wss = new WebSocket.Server({ port: 8080 });
+  
+  setInterval(async () => {
+    await readSensors();
+    //console.log(results)
+  }, 1000);
+
+  app.put('/sensors/:sensorId/high', async (req, res) => {
+    const { sensorId } = req.params;
+    const { high } = req.body;
+
+    if (typeof high !== 'number') {
+      return res.status(400).json({ error: 'Invalid high value' });
+    }
+
+    try {
+      await updateSensorThreshold(sensorId, 'high', high);
+      res.json({ success: true });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+  app.put('/sensors/:sensorId/low', async (req, res) => {
+    const { sensorId } = req.params;
+    const { low } = req.body;
+
+    if (typeof low !== 'number') {
+      return res.status(400).json({ error: 'Invalid low value' });
+    }
+
+    try {
+      await updateSensorThreshold(sensorId, 'low', low);
+      res.json({ success: true });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+  app.put('/sensors/:sensorId/time', async (req, res) => {
+    const { sensorId } = req.params;
+    const { time } = req.body;
+
+    if (typeof time !== 'number') {
+      return res.status(400).json({ error: 'Invalid time value' });
+    }
+
+    try {
+      await updateSensorTime(sensorId, time);
+      res.json({ success: true });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+
+
+  wss.on('connection', (ws) => {
+    console.log('Cliente conectado');
+
+    setInterval(async () => {
+      //await readSensors();
+      ws.send(JSON.stringify(results));
+    }, 1000);
+  });
+
+  // Iniciar el servidor HTTP
+  app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
+
+  socket.on('error', (err) => {
+    console.error('Error de conexión Modbus:', err);
+  });
+}
+
+startServer();
